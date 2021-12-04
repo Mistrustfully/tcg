e8b4e0b817cd4b60d5f4f1eeab75a0957120e200
Mistrustfully <mistrustfully@gmail.com>
2021-12-04 10:37:10 -0500

    ADHD prevents me from doing anything more.

---
+++ src/server/services/cardService.ts
@@ -0,0 +1,35 @@
+import { OnStart, Service } from "@flamework/core";
+import { Players } from "@rbxts/services";
+import cards from "shared/cards.json";
+
+// Knuth-Fisher-Yates shuffle
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array: unknown[]) {
+	let currentIndex = array.size(),
+		randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex !== 0) {
+		// Pick a remaining element...
+		randomIndex = math.floor(math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
+}
+
+@Service()
+export class cardService implements OnStart {
+	createDeck(player: Player) {
+		const deck = [cards.Sunrise, cards.Sunset];
+		shuffle(deck);
+		print(player, deck);
+	}
+
+	onStart() {
+		Players.PlayerAdded.Connect((player) => this.createDeck(player));
+	}
+}
