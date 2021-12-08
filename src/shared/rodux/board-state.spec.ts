debaad386eadf71511c1f0e2c6202876f6d4665d
mistrustfully <mistrustfully@gmail.com>
2021-12-08 00:30:44 -0500

    Refactor tests

--- src/shared/rodux/board-state.spec.ts
+++ src/shared/rodux/board-state.spec.ts
@@ -6,70 +6,37 @@ import { CreateBoardStore, DrawCard, PlayCard } from "./board-state";
 
 export = () => {
 	describe("Board State", () => {
+		const BoardStore = CreateBoardStore({
+			PlayerOne: {
+				Hand: [Cards.Sunrise],
+				Field: [],
+				Deck: [Cards.Sunset],
+				DiscardPile: [Cards.BoilingRain],
+			},
+
+			PlayerTwo: {
+				Hand: [],
+				Field: [],
+				Deck: [],
+				DiscardPile: [],
+			},
+		});
+
 		it("should play a card from the hand.", () => {
-			const BoardStore = CreateBoardStore({
-				PlayerOne: {
-					Hand: [Cards.Sunrise],
-					Field: [],
-					Deck: [],
-					DiscardPile: [],
-				},
-
-				PlayerTwo: {
-					Hand: [Cards.Sunset],
-					Field: [],
-					Deck: [],
-					DiscardPile: [],
-				},
-			});
-
 			BoardStore.dispatch(PlayCard("PlayerOne", 0));
-
 			expect(Llama.List.equals(BoardStore.getState().PlayerOne.Field, [Cards.Sunrise])).to.be.ok();
 		});
 
 		it("should draw a card", () => {
-			const BoardStore = CreateBoardStore({
-				PlayerOne: {
-					Hand: [],
-					Field: [],
-					Deck: [Cards.Sunrise],
-					DiscardPile: [],
-				},
-
-				PlayerTwo: {
-					Hand: [Cards.Sunset],
-					Field: [],
-					Deck: [],
-					DiscardPile: [],
-				},
-			});
-
 			BoardStore.dispatch(DrawCard("PlayerOne"));
-
-			expect(Llama.List.equals(BoardStore.getState().PlayerOne.Hand, [Cards.Sunrise])).to.be.ok();
+			expect(Llama.List.equals(BoardStore.getState().PlayerOne.Hand, [Cards.Sunset])).to.be.ok();
 		});
 
 		it("should shuffle the discard pile into the deck, and draw", () => {
-			const BoardStore = CreateBoardStore({
-				PlayerOne: {
-					Hand: [],
-					Field: [],
-					Deck: [],
-					DiscardPile: [Cards.Sunrise],
-				},
-
-				PlayerTwo: {
-					Hand: [Cards.Sunset],
-					Field: [],
-					Deck: [],
-					DiscardPile: [],
-				},
-			});
-
 			BoardStore.dispatch(DrawCard("PlayerOne"));
-
-			expect(Llama.List.equals(BoardStore.getState().PlayerOne.Hand, [Cards.Sunrise])).to.be.ok();
+			expect(
+				Llama.List.equals(BoardStore.getState().PlayerOne.Hand, [Cards.Sunrise, Cards.BoilingRain]),
+			).to.be.ok();
 		});
 	});
 };
